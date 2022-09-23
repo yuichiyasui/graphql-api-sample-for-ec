4bf9b77d54de1818d383a31230eeb4a11acdf425
yuichiyasui <yassan.b.a.w@gmail.com>
2022-09-23 22:24:14 +0900

    Create initial apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log('Hello World!');
+import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+const typeDefs = gql`
+  type Item {
+    id: ID!
+    name: String!
+  }
+
+  type Query {
+    items: [Item!]!
+  }
+`;
+
+const items = [
+  {
+    id: '1',
+    name: '商品1',
+  },
+  {
+    id: '2',
+    name: '商品2',
+  },
+];
+
+const resolvers = {
+  Query: {
+    items: () => items,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+  /** use `ApolloServerPluginLandingPageProductionDefault` in production,  */
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+});
+
+server.listen().then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀  Server ready at ${url}`);
+});
